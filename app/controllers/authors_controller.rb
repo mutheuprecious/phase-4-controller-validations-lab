54a8ad2376cf3a86f47a57c0d1d5759f7782aa02
Precious <preciousmaregwa1997@gmail.com>
2022-07-21 13:15:18 +0300

    commit tree e2c2817584acce153e31f4e92dd09c321bf71613

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,24 @@
 class AuthorsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    def show
+      author = Author.find(params[:id])
   
-  def show
-    author = Author.find(params[:id])
-
-    render json: author
-  end
-
-  def create
-    author = Author.create(author_params)
-
-    render json: author, status: :created
-  end
-
-  private
+      render json: author
+    end
   
-  def author_params
-    params.permit(:email, :name)
-  end
+    def create
+      author = Author.create!(author_params)
+      render json: author, status: :created
+    end
   
-end
+    private
+    
+    def author_params
+      params.permit(:email, :name)
+    end
+  
+    def render_unprocessable_entity(invalid)
+      render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
+    
+  end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,27 @@
 class PostsController < ApplicationController
-
-  def show
-    post = Post.find(params[:id])
-    
-    render json: post
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    def show
+      post = Post.find(params[:id])
+      
+      render json: post
+    end
+  
+    def update
+      post = Post.find(params[:id])
+  
+      post.update!(post_params)
+  
+      render json: post
+    end
+  
+    private
+  
+    def post_params
+      params.permit(:category, :content, :title)
+    end
+  
+    def render_unprocessable_entity(invalid)
+      render json: { errors: invalid.record.errors.messages }, status: :unprocessable_entity
+    end
+  
   end
-
-  def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
-  end
-
-  private
-
-  def post_params
-    params.permit(:category, :content, :title)
-  end
-
-end
